4a7768303fdcd6afb2d2672f9e9285055023c83e
Danie Conradie <sdconradie@gmail.com>
2018-06-03 18:08:18 +0200

    Line breaks corrected, basic psalm display done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default ({ data }) =>{
+  console.log(data)
+  const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 1));
+  console.log(huidigePsalm)
+  return(
+    <div>
+      <h1>Psalm {huidigePsalm.psalm}</h1>
+      {huidigePsalm.verse.map(({vers, teks}) => (
+        <div>
+          <h3> {vers} </h3>
+          <p> {teks} </p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query PsalmsQuery {
+    srcYaml{
+      psalms{
+        psalm
+        verse{
+          vers
+          teks
+        }
+      }
+    }
+  }
+`;
