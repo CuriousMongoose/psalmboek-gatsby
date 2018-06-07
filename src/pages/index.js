bf178083ed19dbcccbb2e597fd5db72cf11bfc3e
Danie Conradie <danie@dopptech.co.za>
2018-06-07 14:28:52 +0200

    Started configuring state for app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      psalms: data.srcYaml.psalms,
+      huidigePsalm: 1
+    }
+  }
+}
+
+render() {
+  console.log(data)
+  console.log(huidigePsalm)
+  return(
+    <div>
+      <h1>Psalm {huidigePsalm.psalm}</h1>
+      {huidigePsalm.verse.map(({vers, teks}) => (
+        <div>
+          <h3> {vers} </h3>
+          <p> {teks} </p>
+        </div>
+      ))}
+    </div>
+  )
+}
 export default ({ data }) =>{
   console.log(data)
   const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 1));
