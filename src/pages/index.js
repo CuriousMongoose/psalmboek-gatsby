2b7ea2d4b99fa18465e507020db2cc34fe613e4c
Danie Conradie <sdconradie@gmail.com>
2018-06-10 21:19:45 +0200

    Stopping main class work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 class App extends React.Component{
   constructor() {
@@ -7,41 +7,41 @@ class App extends React.Component{
     this.state = {
       psalms: data.srcYaml.psalms,
       huidigePsalm: 1
-    }
+    };
   }
-}
 
-render() {
-  console.log(data)
-  console.log(huidigePsalm)
-  return(
-    <div>
-      <h1>Psalm {huidigePsalm.psalm}</h1>
-      {huidigePsalm.verse.map(({vers, teks}) => (
-        <div>
-          <h3> {vers} </h3>
-          <p> {teks} </p>
-        </div>
-      ))}
-    </div>
-  )
-}
-export default ({ data }) =>{
-  console.log(data)
-  const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 1));
-  console.log(huidigePsalm)
-  return(
-    <div>
-      <h1>Psalm {huidigePsalm.psalm}</h1>
-      {huidigePsalm.verse.map(({vers, teks}) => (
-        <div>
-          <h3> {vers} </h3>
-          <p> {teks} </p>
-        </div>
-      ))}
-    </div>
-  )
-}
+  render() {
+    console.log(data);
+    console.log(huidigePsalm);
+    return(
+      <div>
+        <h1>Psalm {huidigePsalm.psalm}</h1>
+        {huidigePsalm.verse.map(({vers, teks}) => (
+          <div>
+            <h3> {vers} </h3>
+            <p> {teks} </p>
+          </div>
+        ))}
+      </div>
+    )
+  }
+};
+// export default ({ data }) =>{
+//   console.log(data)
+//   const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 1));
+//   console.log(huidigePsalm)
+//   return(
+//     <div>
+//       <h1>Psalm {huidigePsalm.psalm}</h1>
+//       {huidigePsalm.verse.map(({vers, teks}) => (
+//         <div>
+//           <h3> {vers} </h3>
+//           <p> {teks} </p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
 
 export const query = graphql`
   query PsalmsQuery {
