383a9f8e3bd5889a0cb4b36feb1fa53d868b14a7
Danie Conradie <sdconradie@gmail.com>
2018-06-10 22:41:48 +0200

    Sucessfully changed main component to class-based

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-export default ({ data }) =>{
-  console.log(data)
-  const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 1));
-  console.log(huidigePsalm)
-  return(
-    <div>
-      <h1>Psalm {huidigePsalm.psalm}</h1>
-      {huidigePsalm.verse.map(({vers, teks}) => (
-        <div>
-          <h3> {vers} </h3>
-          <p> {teks} </p>
-        </div>
-      ))}
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    const huidigePsalm = this.props.data.srcYaml.psalms.find((ps) => (ps.psalm === 2));
+    console.log(huidigePsalm)
+    return(
+      <div>
+        <h1>Psalm {huidigePsalm.psalm}</h1>
+        {huidigePsalm.verse.map(({vers, teks}) => (
+          <div>
+            <h3> {vers} </h3>
+            <p> {teks} </p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
+
+export default App;
+
+// export default ({ data }) =>{
+//   console.log(data)
+//   const huidigePsalm = data.srcYaml.psalms.find((ps) => (ps.psalm === 2));
+//   console.log(huidigePsalm)
+//   return(
+//     <div>
+//       <h1>Psalm {huidigePsalm.psalm}</h1>
+//       {huidigePsalm.verse.map(({vers, teks}) => (
+//         <div>
+//           <h3> {vers} </h3>
+//           <p> {teks} </p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
 
 export const query = graphql`
   query PsalmsQuery {
